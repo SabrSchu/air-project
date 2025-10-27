9822782a56a8d3cbb7d565f57997cf28d32f490b
Jonas Glaser <jonas.glaser@student.tugraz.at>
2025-10-27 10:28:50 +0100

    introducing 1. Version of PlantCard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
-<!-- Test Screen with some buttons to test accessing our Backend -->
-
 <template>
-  <div style="background-color: white; height: 100vh; display: flex; justify-content: center; align-items: center;">
+  <div class="main-div">
+    <PlantCard />
+  </div>
+  <div class="button-div">
     <TestButton />
     <DataButton />
   </div>
@@ -10,4 +11,21 @@
 <script setup lang="ts">
 import TestButton from './components/TestButton.vue'
 import DataButton from './components/DataButton.vue'
+import PlantCard from './components/PlantCard.vue'
 </script>
+
+<style scoped>
+.main-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.button-div {
+  background-color: white;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/PlantCard.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import DemoPlant from '@/assets/DemoPlant.jpg'
+
+let name = "Monstera";
+let description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum velit vel augue rutrum, in mattis nibh ornare.";
+
+function showMessage() {
+  console.log('Hello World');
+}
+</script>
+
+<template>
+  <div class="plant-card">
+    <img class="plant-image" :src="DemoPlant" alt="Demo Plant" />
+
+    <div class="text-box">
+
+      <h1>{{ name }}</h1>
+
+      <p>
+        {{ description }}
+      </p>
+
+      <button class="button-more-info" @click="showMessage">
+        Let's find out more!
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.plant-card {
+  max-width: 600px;
+  max-height: 200px;
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  border: 2px solid #333;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.plant-image {
+  width: 30%;
+  object-fit: cover;
+  padding: 1.5rem;
+}
+
+.text-box {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.text-box h1 {
+  margin-top: 0.1rem;
+  margin-bottom: 0.25rem;
+}
+
+.text-box p {
+  margin-top: 0.25rem;
+}
+
+.button-more-info {
+  align-self: center;
+  width: 60%;
+  height: 25px;
+  color: white;
+  background-color: darkgreen;
+  border-radius: 10px;
+}
+
+.score-box {
+  display: flex;
+  width:20%;
+  border: black solid 1px;
+}
+</style>
