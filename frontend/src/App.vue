17f0e38771940f39c5fe67a6bfd2f588330f10c0
Eva Haring <eva.haring@student.tugraz.at>
2025-10-30 17:03:31 +0100

    Introducing 1. Version of Plant Overview
    Implements base layout structure using PrimeVue components.
    
    Removes obsolete files.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <NavBar/>
   <Header/>
-  <div class="main-div">
-    <PlantCard
-        v-for="plant in testJson"
-        :key="plant.id"
-    :name="plant.name"
-    :description="plant.description"
-    :water-amount="plant.watering"
-    :sunlight-amount="plant.sunlight"/>
-  </div>
+  <PlantOverview/>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
-*{
+* {
   font-family: 'Poppins', sans-serif;
+}
+
+a {
+  text-decoration: none;
   color: black;
 }
 
@@ -28,28 +24,7 @@ body{
 </style>
 
 <script setup lang="ts">
-import TestButton from './components/TestButton.vue'
-import DataButton from './components/DataButton.vue'
-import PlantCard from './components/PlantCard.vue'
-import testJson from './assets/test-plants.json'
 import NavBar from "@/components/NavBar.vue";
 import Header from "@/components/Header.vue";
+import PlantOverview from "@/components/PlantOverview.vue";
 </script>
-
-<style scoped>
-.main-div {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /*if more then one card*/
-  gap: 0.5rem;
-}
-
-.button-div {
-  background-color: white;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
