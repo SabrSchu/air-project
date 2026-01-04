51695b629d6de7f144e1246fb584d7a00dd52b09
Jonas Glaser <jonas.glaser@student.tugraz.at>
2026-01-04 14:53:23 +0100

    delete not used ChatWindow from App.vue component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,38 +5,13 @@
       <RouterView />
     </main>
     <Footer/>
-
-      <!--
-    <div
-        class="chat-drawer"
-        :class="{ 'is-open': isOpen }"
-        @mouseenter="openChat"
-        @mouseleave="closeChat"
-    >
-      <ChatWindow
-          :isPinned="isPinned"
-          @togglePin="togglePin"
-      />
-    </div>
-    -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
 import {RouterView} from 'vue-router';
 import NavBar from "@/components/NavBar.vue";
 import Footer from "@/components/Footer.vue";
-import ChatWindow from "@/components/ChatWindow.vue";
-
-const isHovered = ref(false);
-const isPinned = ref(false);
-
-const isOpen = computed(() => isHovered.value || isPinned.value);
-
-const openChat = () => { isHovered.value = true; };
-const closeChat = () => { isHovered.value = false; };
-const togglePin = () => { isPinned.value = !isPinned.value; };
 </script>
 
 <style>
@@ -68,27 +43,5 @@ main {
   display: flex;
   flex-direction: column;
   width: 100%;
-}
-
-/* ChatWindow Style */
-.chat-drawer {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  width: 40rem;
-  height: 70vh;
-  border-radius: 1rem;
-  border: 0.1rem solid #969696;
-
-  box-shadow: -0.5rem -0.5rem 2rem rgba(0,0,0,0.1);
-  z-index: 9999;
-
-  /* Animation */
-  transform: translateX(calc(100% - 1rem));
-  transition: transform 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
-}
-
-.chat-drawer.is-open {
-  transform: translateX(0);
 }
 </style>
